434d6c8b8fe4c132ef951be2f91aa60419a4a74e
ryan connolly <connolly-r16@ulster.ac.uk>
2023-08-03 18:09:25 +0100

    Add better text to speech system

--- src/pages/TextToSpeech.tsx
+++ src/pages/TextToSpeech.tsx
@@ -5,6 +5,7 @@ import {
   IonFabButton,
   IonHeader,
   IonIcon,
+  IonImg,
   IonInput,
   IonPage,
   IonTitle,
@@ -13,17 +14,24 @@ import {
 import { useState } from "react";
 import {returnDownBack} from "ionicons/icons";
 import './css/TextToSpeech.css';
+import './css/global.css';
 
 const TextToSpeech: React.FC = () => {
   const [textToBeConverted, setTextToBeConverted] = useState("");
-
+  
   
 
   function convertTextToSpeech() {
 
-    let utter = new SpeechSynthesisUtterance(textToBeConverted);
-    speechSynthesis.speak(utter);
+    let utterText = new SpeechSynthesisUtterance(textToBeConverted);
+    speechSynthesis.speak(utterText);
 
+  }
+
+  function buttonToSpeech(speechValue: string){
+
+    let utterButton = new SpeechSynthesisUtterance(speechValue);
+    speechSynthesis.speak(utterButton);
   }
 
   return (
@@ -35,13 +43,33 @@ const TextToSpeech: React.FC = () => {
       </IonHeader>
 
       <IonContent>
+      
         <div className="main">
+          <IonImg src="assets/icon/logo.png" className="logo" />
           <IonInput
           className="input"
           placeholder="Enter Text"
           onIonChange={(e: any) => setTextToBeConverted(e.target.value)}
           />
-          <IonButton  title="speakButton" expand="block" onClick={convertTextToSpeech} color="secondary">
+
+          <div>
+          <IonButton  title="speakButton"  onClick={() => buttonToSpeech("Hello")} color="secondary">Hello</IonButton>
+          <IonButton  title="speakButton"  onClick={() => buttonToSpeech("Bye")} color="secondary">Bye</IonButton>
+          <IonButton  title="speakButton"  onClick={() => buttonToSpeech("Help")} color="secondary">Help</IonButton>
+          </div>
+          <div>
+          <IonButton  title="speakButton"  onClick={() => buttonToSpeech("Yes")} color="secondary">Yes</IonButton>
+          <IonButton  title="speakButton"  onClick={() => buttonToSpeech("No")} color="secondary">No</IonButton>
+          <IonButton  title="speakButton"  onClick={() => buttonToSpeech("Maybe")} color="secondary">Maybe</IonButton>
+          </div>
+          <div>
+          <IonButton  title="speakButton"  onClick={() => buttonToSpeech("Water")} color="secondary">Water</IonButton>
+          <IonButton  title="speakButton"  onClick={() => buttonToSpeech("Food")} color="secondary">Food</IonButton>
+          <IonButton  title="speakButton"  onClick={() => buttonToSpeech("Toilet")} color="secondary">Toliet</IonButton>
+          </div>
+          
+
+          <IonButton  title="speakButton" expand="block" onClick={convertTextToSpeech} color="tertiary">
             Speak!
           </IonButton>
           
